bf8de96915737de179efbeb1de958bdf6b85ec3f
Ivan <ivuckovi@sfu.ca>
2019-01-15 23:08:31 -0800

    Ivan's Git test

--- app/src/main/java/com/bignerdranch/android/vocabularysudoku/SudokuActivity.java
+++ app/src/main/java/com/bignerdranch/android/vocabularysudoku/SudokuActivity.java
@@ -7,6 +7,7 @@ public class SudokuActivity extends AppCompatActivity {
 
     //Stefan's contribution
     // Andi's Contribution
+    // Ivan's Contribution
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
