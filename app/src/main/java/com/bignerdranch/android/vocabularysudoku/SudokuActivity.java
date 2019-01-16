5611bd307db9a17b0dd9e8c948bb31738f6af5c3
stefanlynka <stefanlynka@yahoo.ca>
2019-01-15 19:41:56 -0800

    Testing Git pulls and pushes

--- app/src/main/java/com/bignerdranch/android/vocabularysudoku/SudokuActivity.java
+++ app/src/main/java/com/bignerdranch/android/vocabularysudoku/SudokuActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class SudokuActivity extends AppCompatActivity {
 
+    //Stefan's contribution
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
