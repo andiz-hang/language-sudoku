31b62fd710b3366a46047d5cb806fdc0d0cbaf4d
vucko <ivuckovi@sfu.ca>
2019-02-01 15:22:31 -0800

    Scaling popup window location

--- app/src/main/java/com/bignerdranch/android/vocabularysudoku/SudokuActivity.java
+++ app/src/main/java/com/bignerdranch/android/vocabularysudoku/SudokuActivity.java
@@ -2,9 +2,11 @@ package com.bignerdranch.android.vocabularysudoku;
 
 import android.animation.ObjectAnimator;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -32,7 +34,7 @@ public class SudokuActivity extends AppCompatActivity {
     Button[] PopUpButtons = new Button[9];
     Language mLanguage1 = new Language("English","one", "two","three","four","five","six","seven","eight","nine");
     Language mLanguage2 = new Language("French","un", "deux","trois","quatre","cinq","sixe","sept","huit","neuf");
-
+    Point size = new Point();
     // On clicking a square we show a screen with buttons with word choices
     // Pressing one of those buttons hides that screen and fills in that square
 
@@ -42,6 +44,10 @@ public class SudokuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sudoku);
 
         // This are the Pop Up Screen Buttons.
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        View pop_up_grid=findViewById(R.id.pop_up_layout);
+        pop_up_grid.setTranslationY(size.y);
         for(int i = 0; i<9; i++) {
             final Button PopUpButton = new Button(this);
             PopUpButton.setText(mLanguage2.Words[i]);
@@ -100,7 +106,7 @@ public class SudokuActivity extends AppCompatActivity {
             Animate(sudoku_view, "translationX", 0f,500);
             Animate(sudoku_view, "translationY", 0f,500);
 
-            Animate(pop_up_view, "translationY", 1800f, 500);
+            Animate(pop_up_view, "translationY", size.y*1f, 500);
             Animate(sudoku_view, "scaleX", 1f, 500);
             Animate(sudoku_view, "scaleY", 1f, 500);
             on_screen = false;
@@ -126,7 +132,7 @@ public class SudokuActivity extends AppCompatActivity {
 
             Log.d("Test", " ");
 
-            Animate(pop_up_view, "translationY", 1000f, 500);
+            Animate(pop_up_view, "translationY", size.y*5/8f, 500);
             Animate(sudoku_view, "scaleX", zoom_scale, 500);
             Animate(sudoku_view, "scaleY", zoom_scale, 500);
             on_screen = true;
