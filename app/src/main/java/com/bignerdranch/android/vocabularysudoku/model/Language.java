83917af204ef8d36eac709c65c4f4b0f9d1df9d3
andiz <andiz@sfu.ca>
2019-03-07 14:52:16 -0800

    Changed folder names and other refactoring

--- app/src/main/java/com/bignerdranch/android/vocabularysudoku/model/Language.java
+++ app/src/main/java/com/bignerdranch/android/vocabularysudoku/model/Language.java
@@ -1,24 +1,22 @@
 package com.bignerdranch.android.vocabularysudoku.model;
 
 public class Language {
-    String name;
-    String[] Words = new String[10];
+    private String      mName;
+    private String[]    mWords = new String[10];
 
     public Language(String name, String word1, String word2, String word3, String word4, String word5, String word6, String word7, String word8, String word9) {
-        this.name = name;
-        Words[0] = "";
-        Words[1] = word1;
-        Words[2] = word2;
-        Words[3] = word3;
-        Words[4] = word4;
-        Words[5] = word5;
-        Words[6] = word6;
-        Words[7] = word7;
-        Words[8] = word8;
-        Words[9] = word9;
+        mName = name;
+        mWords[0] = "";
+        mWords[1] = word1;
+        mWords[2] = word2;
+        mWords[3] = word3;
+        mWords[4] = word4;
+        mWords[5] = word5;
+        mWords[6] = word6;
+        mWords[7] = word7;
+        mWords[8] = word8;
+        mWords[9] = word9;
     }
 
-    public String getWord(int index){
-        return Words[index];
-    }
+    public String getWord(int index) { return mWords[index]; }
 }
